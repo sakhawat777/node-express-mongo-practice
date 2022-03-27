bbdf05f96600d8697fe1ca0fe631e1c0d01d3655
sakhawat777 <biplobbms@gmail.com>
2022-03-27 22:54:04 +0600

    root api and another api done

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
-// call back function
+// Root API and call back function
 app.get('/', (req, res) => {
-	res.send('Thanks for calling me');
+	// res.send('Thanks for calling me');
+	const fruit = {
+		product: 'Ginger',
+		price: '200 TK',
+	};
+	res.send(fruit);
+});
+app.get('/fruits/banana', (req, res) => {
+	res.send({ fruit: 'banana', quantity: 1000, price: 10000 });
 });
 
 app.listen(3000, () => console.log('Listening to port 3000'));
